9fe631f7e21edd844a4c9d33b4054631f7586a8a
Mika6tommy <tommyhas743@gmail.com>
2024-10-01 20:47:11 +0300

    first modif bglg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#050816",
+        primary: "#7b0515d1",
         secondary: "#aaa6c3",
         tertiary: "#151038",
         "black-100": "#100d25",
